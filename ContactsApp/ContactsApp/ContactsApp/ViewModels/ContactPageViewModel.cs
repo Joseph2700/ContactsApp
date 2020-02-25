688b03f48b9ccea5a128c15c86fb71492237d767
Joseph2700 <50160800+Joseph2700@users.noreply.github.com>
2020-02-25 11:27:22 -0600

    Created ContactCollection
    Created de ContactObservableCollection and added a Name and a Number

--- ContactsApp/ContactsApp/ContactsApp/ViewModels/ContactPageViewModel.cs
+++ ContactsApp/ContactsApp/ContactsApp/ViewModels/ContactPageViewModel.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using ContactsApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace ContactsApp.ViewModels
 {
-    class ContactPageViewModel
+    public class ContactPageViewModel
     {
+        public ObservableCollection<Contact> Contacts { get; set; } = new ObservableCollection<Contact>();
+
+        public ContactPageViewModel()
+        {
+            Contacts.Add(new Contact() { Name = "Jose", Number = "8099860592" });
+        }
     }
 }
